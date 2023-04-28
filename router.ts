3228cbb07df7287d4ff9e5fd411ff69b6b7f5c59
Isabella Oliveira <goliveirapk@gmail.com>
2023-04-28 16:10:48 -0300

    refactor: router.serve recives an optional port instead of passing it on instanciation. remove function RouteDir and now doesThat in the router

--- router.ts
+++ router.ts
@@ -4,26 +4,12 @@ import serve, { createStringHashmap } from "./server";
 import type { IDynamicRoutes, StringHashmap } from "./server";
 
 export default class Router {
-	port: number;
 	urls: string[];
 	staticRoutes: StringHashmap;
 	dynamicRoutes: IDynamicRoutes;
 	rootDir: string;
 
-	constructor(dir: string, port?: number) {
-		this.port = port ?? 8080;
-		this.rootDir = dir;
-		const { staticRoutes, urls } = this.routeDir();
-
-		urls.forEach((url) => staticBuilder(staticRoutes, url));
-
-		this.urls = urls;
-		this.staticRoutes = staticRoutes;
-		this.dynamicRoutes = {};
-	}
-	private routeDir() {
-		const dir = this.rootDir;
-
+	constructor(dir: string) {
 		const files = mapFilesIn(dir);
 		const urls = files.map((file) =>
 			file.replace(dir, "").replace("index.html", "")
@@ -31,7 +17,12 @@ export default class Router {
 
 		const routes = createStringHashmap(urls, files);
 
-		return { staticRoutes: routes, urls };
+		urls.forEach((url) => staticBuilder(routes, url));
+
+		this.rootDir = dir;
+		this.urls = urls;
+		this.staticRoutes = routes;
+		this.dynamicRoutes = {};
 	}
 
 	routeFallback(url: string, func: (arg: string) => StringHashmap) {
@@ -41,8 +32,8 @@ export default class Router {
 		this.dynamicRoutes[url] = { file, func };
 	}
 
-	serve() {
+	serve(port?: number) {
 		const { staticRoutes, dynamicRoutes } = this;
-		serve({ staticRoutes, dynamicRoutes }, this.port);
+		serve({ staticRoutes, dynamicRoutes }, port ?? 8080);
 	}
 }
